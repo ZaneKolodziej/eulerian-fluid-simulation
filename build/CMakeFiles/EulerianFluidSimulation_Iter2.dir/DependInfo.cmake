3d378b4453da7dae10bd42abe599d7f8177cca5d
ZaneKolodziej <zanekolo@icloud.com>
2026-08-06 18:45:42 -0400

    iter-2: working CMake build, blank entry point

---
+++ build/CMakeFiles/EulerianFluidSimulation_Iter2.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/external/glad/src/glad.c" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/glad/src/glad.c.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/glad/src/glad.c.obj.d"
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/external/ImGui/backends/imgui_impl_glfw.cpp" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/backends/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/backends/imgui_impl_glfw.cpp.obj.d"
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/external/ImGui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/backends/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/external/ImGui/imgui.cpp" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui.cpp.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui.cpp.obj.d"
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/external/ImGui/imgui_draw.cpp" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui_draw.cpp.obj.d"
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/external/ImGui/imgui_tables.cpp" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui_tables.cpp.obj.d"
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/external/ImGui/imgui_widgets.cpp" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/external/ImGui/imgui_widgets.cpp.obj.d"
+  "C:/Users/Zane/dev-projects/EulerianFluidSimulation_Iter2/src/main.cpp" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/src/main.cpp.obj" "gcc" "CMakeFiles/EulerianFluidSimulation_Iter2.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/EulerianFluidSimulation_Iter2.dir/src/main.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/EulerianFluidSimulation_Iter2.dir/src/main.cpp.obj: \
+ C:\Users\Zane\dev-projects\EulerianFluidSimulation_Iter2\src\main.cpp
